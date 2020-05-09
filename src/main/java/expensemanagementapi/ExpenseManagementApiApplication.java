400ae24a46b1bc45600bef91e61aaabe10e7b506
Marcos <mt>
2020-05-09 09:52:36 -0300

    init project

---
+++ src/main/java/expensemanagementapi/ExpenseManagementApiApplication.java
@@ -0,0 +1,13 @@
+package expensemanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseManagementApiApplication.class, args);
+	}
+
+}
